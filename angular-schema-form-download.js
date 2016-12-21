53bea401ad759333a1493372a3d76eb177e6ff62
Rafi Ali Khan <rafialikhan@gmail.com>
2016-12-22 00:55:09 +0530

    Actually added Schema default

--- angular-schema-form-download.js
+++ angular-schema-form-download.js
@@ -1,8 +1,19 @@
 angular.module("schemaForm").run(["$templateCache", function($templateCache) {$templateCache.put("directives/decorators/bootstrap/download/angular-schema-form-download.html","<div class=\"form-group\" ng-class=\"{\'has-error\': hasError()}\">\n    <div>\n       <a class=\"btn btn-default btn-primary\" download href=\"{{form.path}}\" role=\"button\">{{form.title || \'Download\'}}</a>\n    </div>\n    <span class=\"help-block\">{{ (hasError() && errorMessage(schemaError())) || form.description}}</span>\n</div>");}]);
 angular.module('schemaForm').config(
     ['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfPathProvider',
-        function(schemaFormProvider,  schemaFormDecoratorsProvider) {
+        function(schemaFormProvider,  schemaFormDecoratorsProvider, sfPathProvider) {
 
+          var download = function(name, schema, options) {
+            if (schema.type === 'download' || schema.format === 'download') {
+              var f = schemaFormProvider.stdFormObj(name, schema, options);
+              f.key  = options.path;
+              f.type = 'download';
+              options.lookup[sfPathProvider.stringify(options.path)] = f;
+              return f;
+            }
+          };
+
+          schemaFormProvider.defaults.string.unshift(download);
           //Add to the bootstrap directive
           schemaFormDecoratorsProvider.addMapping(
               'bootstrapDecorator',
--- src/angular-schema-form-download.js
+++ src/angular-schema-form-download.js
@@ -1,7 +1,18 @@
 angular.module('schemaForm').config(
     ['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfPathProvider',
-        function(schemaFormProvider,  schemaFormDecoratorsProvider) {
+        function(schemaFormProvider,  schemaFormDecoratorsProvider, sfPathProvider) {
 
+          var download = function(name, schema, options) {
+            if (schema.type === 'download' || schema.format === 'download') {
+              var f = schemaFormProvider.stdFormObj(name, schema, options);
+              f.key  = options.path;
+              f.type = 'download';
+              options.lookup[sfPathProvider.stringify(options.path)] = f;
+              return f;
+            }
+          };
+
+          schemaFormProvider.defaults.string.unshift(download);
           //Add to the bootstrap directive
           schemaFormDecoratorsProvider.addMapping(
               'bootstrapDecorator',
