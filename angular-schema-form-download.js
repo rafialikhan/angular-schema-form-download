08bc6335148d171c9b63c617ccac2846a201e7ac
Rafi Ali Khan <rafialikhan@gmail.com>
2016-12-22 01:04:18 +0530

    Updating to standard format=download

--- angular-schema-form-download.js
+++ angular-schema-form-download.js
@@ -1,10 +1,10 @@
-angular.module("schemaForm").run(["$templateCache", function($templateCache) {$templateCache.put("directives/decorators/bootstrap/download/angular-schema-form-download.html","<div class=\"form-group\" ng-class=\"{\'has-error\': hasError()}\">\n    <div>\n       <a class=\"btn btn-default btn-primary\" download href=\"{{form.path}}\" role=\"button\">{{form.title || \'Download\'}}</a>\n    </div>\n    <span class=\"help-block\">{{ (hasError() && errorMessage(schemaError())) || form.description}}</span>\n</div>");}]);
+angular.module("schemaForm").run(["$templateCache", function($templateCache) {$templateCache.put("directives/decorators/bootstrap/download/angular-schema-form-download.html","<div class=\"form-group\" ng-class=\"{\'has-error\': hasError()}\">\n    <div>\n       <a class=\"btn btn-default btn-primary\" download href=\"{{form.schema.url}}\" role=\"button\">{{form.title || \'Download\'}}</a>\n    </div>\n    <span class=\"help-block\">{{ (hasError() && errorMessage(schemaError())) || form.description}}</span>\n</div>");}]);
 angular.module('schemaForm').config(
     ['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfPathProvider',
         function(schemaFormProvider,  schemaFormDecoratorsProvider, sfPathProvider) {
 
           var download = function(name, schema, options) {
-            if (schema.type === 'download' || schema.format === 'download') {
+            if (schema.type === 'string' || schema.format === 'download') {
               var f = schemaFormProvider.stdFormObj(name, schema, options);
               f.key  = options.path;
               f.type = 'download';
